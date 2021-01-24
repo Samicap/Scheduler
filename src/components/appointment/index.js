264a7deae4058ea3ae00dcb4e93f3342cfbc4e55
samicap <lovestochasewindmills@gmail.com>
2021-01-23 23:15:33 -0500

    added to appointment component to change display based on mode

--- src/components/appointment/index.js
+++ src/components/appointment/index.js
@@ -3,22 +3,42 @@ import Header from "components/appointment/Header";
 import Empty from "components/appointment/Empty";
 import Show from "components/appointment/Show";
 import "components/appointment/styles.scss";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+  const onAdd = () => transition(CREATE);
 
   return (
-  <article className="appointment">
-    <Header 
-    time={props.time}
-    />
-    {props.interview ? 
-      <Show 
-        student={props.interview.student}
-        interviewer={props.interview.interviewer.name}/> 
-        : <Empty />}
-
-
-  </article>
-  
+    <article className="appointment">
+      <Header time={props.time} />
+      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer.name}
+          // onEdit={action("onEdit")}
+          // onDelete={action("onDelete")}
+        />
+      )}
+      { mode === CREATE && (
+        <Form
+          name={"Sam"}
+          interviewer={null}
+          interviewers={props.interviewers}
+          onCancel={back}
+          // onSave={onSave}
+        />
+      )}
+    </article>
   );
 }
+
